0765f85b547aff987b314e8a0a11b2513756e618
Ben Kelly <Benjamin.J.Kelly@live.com>
2022-10-09 18:54:09 -0400

    Ingest .pdf files

--- parse/ingestor_interface.py
+++ parse/ingestor_interface.py
@@ -10,9 +10,23 @@ class ingestor_interface(ABC):
     def can_ingest(self, cls, path: str):
         pass
 
-    @abstractmethod
-    def parse(self, cls, path: str):
-        pass
+    def parse(self, path: str, file_delim: str):
+        quotes = []
+        print("In sup????")
+
+        with open(path, 'r') as f:
+            for line in f:
+                if(len(line.strip())):
+                    #body, author = line.replace('"','').split(" - ")
+                    body, author = line.replace('"','').split(file_delim)
+                    print(f"line is {line}")
+                    print(f"body is {body}")
+                    print(f"author is {author}")
+                    quotes.append((body, author.rstrip()))
+        print("END!!!")
+        for q in quotes:
+            print(f"q is {q}")
+        return quotes
 
 
 class txt_ingestor(ingestor_interface):
@@ -25,24 +39,33 @@ class txt_ingestor(ingestor_interface):
         else:
             return False
 
+    def parse(self, path: str, file_delim: str):
+        super().parse(path, file_delim)
+
+class pdf_ingestor(ingestor_interface):
+
+    def can_ingest(self, path: str):
+        file_type = subprocess.run(['file', path], stdout=subprocess.PIPE).stdout.decode('utf-8').split(' ')[1]
+        if(file_type == "PDF"):
+            return True
+        else:
+            return False
+
     def parse(self, path: str):
-        quotes = []
-        with open(path, 'r') as f:
-            for line in f:
-                if(len(line.strip())):
-                    body, author = line.replace('"','').split(" - ")
-                    print(f"line is {line}")
-                    print(f"body is {body}")
-                    print(f"author is {author}")
-                    quotes.append((body, author.rstrip()))
-        print("END!!!")
-        for q in quotes:
-            print(f"q is {q}")
-        return quotes
+        file_delim = " - "
+        print(f"the path is {path}")
+        new_file = path.replace(".pdf",".txt")
+        old_filename = path.split("/")[-1]
+        print(f"the filename is {old_filename}")
+        print(f"the new file is {new_file}")
+        call = subprocess.run(['pdftotext', path, new_file])
+        super().parse(new_file, file_delim)
+
+
 
 
 if __name__ == "__main__":
-    _txt = txt_ingestor()
-    _txt.can_ingest("/home/kelbenj/Udacity/src/_data/SimpleLines/SimpleLines.txt")
-    if(_txt):
-        _txt.parse("/home/kelbenj/Udacity/src/_data/SimpleLines/SimpleLines.txt")
+    _pdf = pdf_ingestor()
+    _pdf.can_ingest("/home/kelbenj/Udacity/src/_data/DogQuotes/DogQuotesPDF.pdf")
+    if(_pdf):
+        _pdf.parse("/home/kelbenj/Udacity/src/_data/DogQuotes/DogQuotesPDF.pdf")
