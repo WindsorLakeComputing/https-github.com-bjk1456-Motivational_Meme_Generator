c7e6ec8019fd60cc1a6e1b628879f8bb24096b88
Ben Kelly <Benjamin.J.Kelly@live.com>
2022-10-10 13:51:25 -0400

    Ingest csv files

--- parse/ingestor_interface.py
+++ parse/ingestor_interface.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import subprocess
+import re
 #p = subprocess.run(['emoj', 'dog'], stdout=subprocess.PIPE)
 #emoji = p.stdout.decode('utf-8').split(' ')
 
@@ -82,10 +83,44 @@ class docx_ingestor(ingestor_interface):
         call = subprocess.run(['docx2txt', path, new_file])
         super().parse(new_file, file_delim)
 
+class csv_ingestor(ingestor_interface):
+
+    def can_ingest(self, path: str):
+        filename = path.split("/")[-1]
+        file_suffix = filename[-3:].rstrip()
+        print(f"the filename is {filename}")
+        print(f"the file_suffix is {file_suffix}")
+
+        file_type = subprocess.run(['file', path], stdout=subprocess.PIPE).stdout.decode('utf-8').split(': ')[1].rstrip()
+        print(f"the file_type {file_type}")
+
+        if((file_suffix == "csv") and ("ASCII text" in file_type)):
+            return True
+        else:
+            return False
+
+    def parse(self, path: str):
+        print("YEEEHAW")
+
+        with open(path, 'r') as f:
+            for line in f:
+                if(len(line.strip())):
+                    #body, author = line.replace('"','').split(" - ")
+
+                    #print(re.split(‘; |, | \ * | \n’, my_st))
+                    print(f"line is {line}")
+                    if("\"" in line):
+                        body = re.search('\".*\"', line)
+                        #print(f"line {body.group(1)}")
+                        new_str = body.group(0).replace("\"","")
+                        print(f"new_str is {new_str}")
+                        new_lis = list(line.split(","))
+                        print(f"f the author is {new_lis[-1]}")
+        print("END!!!")
+
 
 
 if __name__ == "__main__":
-    _docx = docx_ingestor()
-    _docx.can_ingest("/home/kelbenj/Udacity/src/_data/DogQuotes/DogQuotesDOCX.docx")
-    if(_docx):
-        _docx.parse("/home/kelbenj/Udacity/src/_data/DogQuotes/DogQuotesDOCX.docx")
+    _csv = csv_ingestor()
+    if(_csv.can_ingest("/home/kelbenj/Udacity/src/_data/DogQuotes/DogQuotesCSV.csv")):
+        _csv.parse("/home/kelbenj/Udacity/src/_data/DogQuotes/DogQuotesCSV.csv")
